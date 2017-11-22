478a9e1374dcdcb7cb17152e45174442be913a49
Peggy Lindner <plindner@uh.edu>
2017-11-22 12:53:27 -0600

    - fixed startup with bootstrap live sites fixes #11

--- server/lib/startup.js
+++ server/lib/startup.js
@@ -5,18 +5,20 @@ logger = Winston;
 
 logger.info(`Winston logs are being captured on the console for host: ${require('os').hostname()}.`);
 
-Meteor.startup(function() {
-  // Insert sample data if the live site collection is empty
-  if (LiveSites.find().count() === 0) {
-    JSON.parse(Assets.getText('livesites.json')).site.forEach(function(doc) {
-      LiveSites.insert(doc);
-    });
-  }
-
-  // Create directory for outgoing files for tomorrow
-  fs.mkdirs(`/hnet/outgoing/${moment().year()}/${moment().month() + 1}/${moment().date() + 1}`, function (err) {
-    if (err) {
-      logger.error(err);
+if (Meteor.isServer) {
+  Meteor.startup(() => {
+    // Insert sample data if the live site collection is empty
+    if (LiveSites.find().count() === 0) {
+      JSON.parse(Assets.getText('livesites.json')).forEach((doc) => {
+        LiveSites.insert(doc);
+      });
     }
+
+    // Create directory for outgoing files for tomorrow
+    fs.mkdirs(`/hnet/outgoing/${moment().year()}/${moment().month() + 1}/${moment().date() + 1}`, function (err) {
+      if (err) {
+        logger.error(err);
+      }
+    });
   });
-});
+}
